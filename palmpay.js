896dee92d0c70fb5e174ea16274df6685ed5a7b8
ideejoy <92212530+ideejoy@users.noreply.github.com>
2021-10-29 20:41:39 +0100

    html/css and javascripts

---
+++ palmpay.js
@@ -0,0 +1,36 @@
+document.getElementById("limg1").onclick = function(){
+alert("No Money");
+}
+document.getElementById("limg2").onclick = function(){
+    alert("No Discount");
+    }
+document.getElementById("limg3").onclick = function(){
+    alert("No Airtime");
+        }
+document.getElementById("limg4").onclick = function(){
+    alert("No Data");
+       }
+document.getElementById("limg5").onclick = function(){
+    alert("No Payment");
+        }
+document.getElementById("limg6").onclick = function(){
+    alert("No Overdraft");
+        }
+document.getElementById("limg7").onclick = function(){
+   alert("No Betting");
+        }
+document.getElementById("limg8").onclick = function(){
+    alert("No Referals");
+}  
+document.getElementById("limg9").onclick = function(){
+    alert("No Shopana");
+}   
+document.getElementById("limg10").onclick = function(){
+    alert("You Cant Check-in");
+    }
+    document.getElementById("limg11").onclick = function(){
+        alert("Nothing to Grab");
+}  
+document.getElementById("limg12").onclick = function(){
+    alert("MORE NOT YET");
+    } 
